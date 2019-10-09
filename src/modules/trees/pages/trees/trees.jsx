ca3660a0d3341add590323a1ecb9a623dfec9474
Brian Ramirez <brians@uninorte.edu.co>
2019-10-09 11:10:03 -0500

    fix: Use switch in routers components

--- src/modules/trees/pages/trees/trees.jsx
+++ src/modules/trees/pages/trees/trees.jsx
@@ -2,17 +2,21 @@ import {
   Box,
 } from '@material-ui/core';
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import styles from './trees.module.scss';
 import TreesIndex from '../../components/trees-index/trees-index';
 import TreeDetail from '../../components/tree-detail/tree-detail';
+import TreeQrReader from '../../components/tree-qr-reader/tree-qr-reader';
 
 function Trees({ match }) {
   return (
     <Box className={styles.wrapper}>
-      <Route exact path={`${match.url}`} component={TreesIndex} />
-      <Route exact path={`${match.url}/:publicCode`} component={TreeDetail} />
+      <Switch>
+        <Route exact path={`${match.url}`} component={TreesIndex} />
+        <Route exact path={`${match.url}/search`} component={TreeQrReader} />
+        <Route exact path={`${match.url}/:publicCode`} component={TreeDetail} />
+      </Switch>
     </Box>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import './home.scss';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Welcome } from '../../modules/static';
 import { Trees } from '../../modules/trees';
 import styles from './home.module.scss';
@@ -8,8 +8,10 @@ import styles from './home.module.scss';
 function Home() {
   return (
     <div className={styles.wrapper}>
-      <Route exact path="/" component={Welcome} />
-      <Route path="/trees" component={Trees} />
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/trees" component={Trees} />
+      </Switch>
     </div>
   );
 }
