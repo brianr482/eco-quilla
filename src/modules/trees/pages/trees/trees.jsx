335b05e731603f67071ac3273ecdf8a85e664234
Brian Ramirez <brians@uninorte.edu.co>
2019-10-09 09:38:19 -0500

    fix: Implement trees module nested route by match   - Use match prop in order to handle this

--- src/modules/trees/pages/trees/trees.jsx
+++ src/modules/trees/pages/trees/trees.jsx
@@ -3,15 +3,22 @@ import {
 } from '@material-ui/core';
 import React from 'react';
 import { Route } from 'react-router-dom';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import styles from './trees.module.scss';
 import TreesIndex from '../../components/trees-index/trees-index';
+import TreeDetail from '../../components/tree-detail/tree-detail';
 
-function Trees() {
+function Trees({ match }) {
   return (
     <Box className={styles.wrapper}>
-      <Route path="/" component={TreesIndex} />
+      <Route exact path={`${match.url}`} component={TreesIndex} />
+      <Route exact path={`${match.url}/:publicCode`} component={TreeDetail} />
     </Box>
   );
 }
+
+Trees.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
+};
 
 export default Trees;
